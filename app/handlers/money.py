6aebf777bb4330660da5754ff1ad239b6c0601d3
malyukd <daria.malyuk@gmail.com>
2024-06-13 02:41:54 +0300

    уьрала ненужные импорты

--- app/handlers/money.py
+++ app/handlers/money.py
@@ -1,11 +1,6 @@
-import asyncio
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram import Router, F
 from app.keyboards import *
-from app.images import file_ids
+
 
 router_money = Router()
 
